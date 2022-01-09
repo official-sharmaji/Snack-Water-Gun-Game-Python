daeb9ec3b1b24a334cbebac0bf4860f05ddcb647
Ranjan Sharma <83359875+official-sharmaji@users.noreply.github.com>
2022-01-09 22:42:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+
+
+def gameWin(comp, you):
+    if comp == you:
+        return None
+    elif comp == 's':
+        if you== 'w':
+            return False
+        elif you == 'g':
+            return True
+    elif comp == 'w':
+        if you== 'g':
+            return False
+        elif you == 's':
+            return True
+    elif comp == 'g':
+        if you== 's':
+            return False
+        elif you == 'w':
+            return True
+
+print("comp Turn: Snack(s) Water(w) Gun(g)?")
+randNo = random.randint(1,3)
+if randNo == 1:
+    comp = 's'
+elif randNo == 2:
+    comp = 'w'
+elif randNo == 3:
+    comp = 'g'
+
+you = input("Your Turn: Snack(s) Water(w) Gun(g)?")
+a = gameWin(comp, you)
+if a == None:
+    print("The game is tie!")
+elif a:
+    print("you Win!")
+else:
+    print("you lose!")
